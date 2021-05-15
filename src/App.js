23dc525c8819f166b61426a1517d26b319088326
Aileen CM <aileenchany@gmail.com>
2021-05-15 14:43:44 -0400

    Added footer with link to GitHub repository
    Added footer with link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
           <h1>Weather App</h1>
         </div>
       </div>
+      <footer><a href="https://github.com/aileenchany/react-weather-app" target="_blank">Open-source</a> code by <a href="https://www.linkedin.com/in/aileen-chanmiranda/" target="_blank">ACM</a></footer>
     </div>
   );
 }
