19a52a1b57429587482e64f81c89955e53cc8c9b
Aileen CM <aileenchany@gmail.com>
2021-05-19 16:46:33 -0400

    Added API integration for the weekly forecast and created the DailyForecast component
    Added API integration for the weekly forecast, stored forecast data inside a state manager, and created the DailyForecast component with single day data as props.

---
+++ src/DailyForecast.js
@@ -0,0 +1,31 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon.js";
+
+export default function DailyForecast(props) {
+
+    function maxTemp() {
+        let temperature = Math.round(props.data.temp.max);
+        return `${temperature}°`;
+    }
+
+    function minTemp() {
+        let temperature = Math.round(props.data.temp.min);
+        return `${temperature}°`;
+    }
+
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+        let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        
+        return days[day];
+    }
+
+    return (
+        <div>
+            <div className="week-day">{day()}</div>
+            <WeatherIcon code={props.data.weather[0].icon} size={45} />
+            <div><span className="max-temp">{maxTemp()}</span><span className="min-temp">{minTemp()}</span></div>
+        </div>
+    );
+}
--- src/WeeklyForecast.js
+++ src/WeeklyForecast.js
@@ -1,24 +1,30 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon.js";
+import React, {useState} from "react";
+import DailyForecast from "./DailyForecast.js";
 import axios from "axios";
 import "./WeeklyForecast.css";
 
 export default function WeeklyForecast(props) {
-    function handleResponse(response) {
-        console.log(response.data);
-    }
-    
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apiKey = "f9ed2779c7a88244e3c6c97a1ad830b5";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
+    const [loaded, setLoaded] = useState(false);
+    const [forecast, setForecast] = useState(null);
 
-    return(
-        <div className="WeeklyForecast">
-            <div className="week-day">Wed</div>
-            <WeatherIcon code="01d" size={45} />
-            <div><span className="max-temp">89°</span><span className="min-temp">80°</span></div>
-        </div>
-    ) ;
+    function handleResponse(response) {
+        setForecast(response.data.daily); //here we store the weekly forecast inside the "forecast" state manager
+        setLoaded(true);
+    }
+
+    if(loaded) {
+        return(
+            <div className="WeeklyForecast">
+                <DailyForecast data={forecast[0]} />
+            </div>
+         ) ;
+    } else {
+        let latitude = props.coordinates.lat;
+        let longitude = props.coordinates.lon;
+        let apiKey = "f9ed2779c7a88244e3c6c97a1ad830b5";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return null;
+    }
 }
